6ccf442b2c8890e7d3e8cf32a14d67fdc721820a
Victor Elgersma <victor.elgersma41@thg.com>
2025-01-26 16:27:19 +0000

    make server rendered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lambent-jalebi-200841.netlify.app/",
+  output: "server",
+  adapter: netlify(),
+});
