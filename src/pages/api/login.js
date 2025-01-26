77654c98808e9fde5be53082b927f6b37fd1b2a4
Victor Elgersma <victor.elgersma41@thg.com>
2025-01-26 16:21:05 +0000

    npm i pocketbase:

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,13 +1,17 @@
 // src/pages/api/login.js (or .ts)
 import PocketBase from "pocketbase";
 import { APIRoute } from "astro";
+const pocketbase_instance = import.meta.env.POCKETBASE_INSTANCE;
 
 export const POST = async ({ request }) => {
   // Parse the request body
-  const { email, password } = await request.json();
+  // const { email, password } = await request.json();
+
+  const email = "test@example.com";
+  const password = "password";
 
   // Initialize PocketBase client
-  const pb = new PocketBase("https://your-pocketbase-url.com");
+  const pb = new PocketBase(pocketbase_instance);
 
   try {
     // Authenticate the user with PocketBase
