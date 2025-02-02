97a20ce3fe2a77712f85cc979f31729ca20bd798
Victor Elgersma <victor.elgersma41@thg.com>
2025-02-02 15:24:11 +0000

    loggin in with pocketbase, setting opaque cookie

---
+++ src/pages/api/login.js
@@ -0,0 +1,30 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("https://be-proud-thunder-3266.fly.dev");
+//elger@gmail.com
+//Bemetr7#12
+// good credentials
+export const POST = async ({ request, cookies, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  try {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(email, password);
+    const token = authData.token;
+    const maxAge = parseInt(import.meta.env.MAX_AGE, 10) || 604800;
+
+    cookies.set(import.meta.env.COOKIE_NAME || "_Security_Login_", token, {
+      path: "/",
+      maxAge,
+      secure: Boolean(import.meta.env.COOKIE_SECURE || false),
+    });
+
+    return redirect("/");
+  } catch (error) {
+    console.log(error);
+    return redirect("/login?error=invalid_credentials");
+  }
+};
